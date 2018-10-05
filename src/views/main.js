5c4f9ff4d0e6d1159dcc8e34a05b30dd9e88d8c1
Hunor Karamán <hunix94@gmail.com>
2018-10-05 23:44:02 +0200

    removed setinterval

--- src/views/main.js
+++ src/views/main.js
@@ -8,10 +8,6 @@ const playlist = new Playlist()
 module.exports = view
 
 function view(state, emit) {
-	setInterval(() => {
-		playlist.next()
-	}, 3000)
-
 	return html`
 		<body class="fl w-100 pa5 sans-serif f3 lh-copy">
 			<div class="w-100 mw7 center">
