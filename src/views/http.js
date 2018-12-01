29e8c23a5d9a0b08baf176cb37e5042ad8c49a6b
Alex Singh <alex@alexsingh.com>
2018-12-01 12:12:30 +1100

    Change <title> to Soundscape

--- src/views/http.js
+++ src/views/http.js
@@ -8,7 +8,7 @@ function view(state, emit) {
 	return html`
 		<body class="fl w-100 pa5 sans-serif f3 lh-copy">
 			<div class="w-100">
-				<div class="mb5">HANGTIME</div>
+				<div class="mb5">Soundscape</div>
 				<div>PEER-TO-PEER ONLY APPLICATION. OPEN IT <a href="https://beakerbrowser.com">BEAKER BROWSER</a>.</div>
 			</div>
 		</body>
