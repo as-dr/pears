b0e6701cce7879b930629ece7586a6a33e048c5c
Alex Singh <alex@alexsingh.com>
2018-12-02 15:10:16 +1100

    Updated main view, added non-functioning exit button, updated alignment

--- src/views/main.js
+++ src/views/main.js
@@ -11,17 +11,23 @@ module.exports = view
 
 function view(state, emit) {
 	return html`
-		<body class="w-100 pa5 sans-serif f3 lh-copy">
+		<body class="flex flex-column w-100 pa5 f3 lh-copy items-center">
 			<div class="flex flex-row w-100 mw7 justify-between">
+				<a href="#" class="link flex ph3 pv1 ba color-inherit" onclick="${back}">EXIT</a>
 				<div class="">${state.hangtime.peers.length + 1} LISTENER${state.hangtime.peers.length != 0 ? 'S' : ''}</div>
-				<a href="#" class="link ph3 pv1 color-inherit" onclick="${show_uploader}">ADD SONG</a>
+				<a href="#" class="link ba ph3 pv1 color-inherit" onclick="${show_uploader}">ADD SONG</a>
 			</div>
-			<div class="flex flex-column w-100 mw7">
+			<div class="flex flex-column w-100 mw7 mt5">
 				${playlist.render(state.hangtime.list, state.hangtime.position)}
 			</div>
 			${fileupload.render(emit)}
 		</body>
 	`
+
+	function back(e) {
+		e.preventDefault()
+		emit('setup:prevstep')
+	}
 
 	function show_uploader(e) {
 		e.preventDefault()
