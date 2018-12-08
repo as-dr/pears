49ebdcc9440ab77b33c7b213c2d6337a96493ace
Alex Singh <alex@alexsingh.com>
2018-12-08 12:22:16 +1100

    Initial styling updates to align with new UI

--- src/views/main.js
+++ src/views/main.js
@@ -11,16 +11,21 @@ module.exports = view
 
 function view(state, emit) {
 	return html`
-		<body class="flex flex-column w-100 pa5 f3 lh-copy items-center" ondragover="${showUploader}">
-			<div class="flex flex-row w-100 mw7 justify-between">
-				<a href="#" class="link flex ph3 pv1 ba color-inherit" onclick="${back}">EXIT</a>
-				<div class="flex items-center">${state.hangtime.peers.length + 1} LISTENER${state.hangtime.peers.length != 0 ? 'S' : ''}</div>
-        <a href="#" class="link ba ph3 pv1 color-inherit" onclick="${toggleMute}">${state.hangtime.muted ? 'UNMUTE' : 'MUTE'}</a>
-        <a href="#" class="link ba ph3 pv1 color-inherit" onclick="${showUploader}">ADD SONG</a>
+		<body class="flex flex-row w-100 pv4 ph3 f3 lh-copy items-center" ondragover="${showUploader}">
+			<div class="flex flex-column w-100 mw5 items-center justify-center">
+				<a href="#" class="link flex ph3 pv1 color-inherit" onclick="${back}">Leave</a>
 			</div>
+			<div class="flex flex-column justify-start items-start w-100 h-100">
+				<div class="flex flex-column items-start mv4">
+				${state.hangtime.peers.length + 1} LISTENER${state.hangtime.peers.length != 0 ? 'S' : ''}
+				<a href="#" class="link f3 deep-purple mv2  br1">+ Invite Friends</a>
+				</div>
+        <a href="#" class="link f3 deep-purple " onclick="${toggleMute}">${state.hangtime.muted ? 'Unmute' : 'Mute'}</a>
+        <a href="#" class="link f3 deep-purple" onclick="${showUploader}">+ Add Song</a>
 			<div class="flex flex-column w-100 mw7 mt5">
 				${playlist.render(state.hangtime.list, state.hangtime.position)}
 			</div>
+		</div>
 			${fileupload.render(emit)}
 		</body>
 	`
