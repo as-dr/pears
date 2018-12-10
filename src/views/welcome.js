eba53db82db4df963125e569b2aaaf9dd666c433
Alex Singh <alex@alexsingh.com>
2018-12-10 13:58:19 +1100

    Added HELP and CREDITS section on landing page

--- src/views/welcome.js
+++ src/views/welcome.js
@@ -16,17 +16,67 @@ function view(state, emit) {
 		return html`
 			<body class="w-100 min-vh-100 pa4 lh-copy">
 				<div class="flex flex-column items-center justify-around w-100 min-vh-100">
+					<div class="flex flex-column items-start justify-between w-100 mw7 tl ">
+					  <h2 class="f3 ttu uppercase dark-gray mv0">Listen to music with far away friends.</h2>
+						<h2 class="f3 dark-gray mv0"></h2>
+						<h2 class="f4 gray mv3">Soundscape is a temporary, collaborative playlist.</h2>
+					</div>
+					<div class="flex flex-column items-start justify-start w-100 mw7 ">
+						<a href="#" class="flex f3 deep-purple link mv3 " onclick="${fork}">Create your space.</a>
+						<a href="#" class="flex f3 deep-purple link mv3" onclick="${next}">Join a friend's space.</a>
+					</div>
+				</div>
+				<div class="flex flex-column items-center justify-around w-100 min-vh-100">
 				<div class="flex flex-column items-start justify-between w-100 mw7 tl ">
-				  <h2 class="f3 ttu uppercase dark-gray mv0">Listen to music with far away friends.</h2>
-					<h2 class="f3 dark-gray mv0"></h2>
-					<h2 class="f4 gray mv3">Soundscape is a temporary, collaborative playlist.</h2>
-				</div>
-				<div class="flex flex-column items-start justify-start w-100 mw7 ">
-					<a href="#" class="flex f3 deep-purple link mv3 " onclick="${fork}">Create your space.</a>
-					<a href="#" class="flex f3 deep-purple link mv3" onclick="${next}">Join a friend's space.</a>
-
-				</div>
-
+				<h2 class="f3 ttu uppercase dark-gray mv0">Help</h2>
+				<h2 class="f3 gray mv0">Select a question to see the answer</h2>
+					<ul class="list pl0 dark-gray">
+						<ul class="list pl0 mv4">
+						<li class="f3 deep-purple pointer">What is this?</li>
+						<li class="dn f4">Answer</li>
+						</ul>
+						<ul class="list pl0 mv4">
+						<li class="f3 deep-purple pointer">What's DAT?</li>
+						<li class="dn f4">Answer</li>
+						</ul>
+						<ul class="list pl0 mv4">
+						<li class="f3 deep-purple pointer">Why do I need to use Beaker Browser?</li>
+						<li class="dn f4">Answer</li>
+						</ul>
+						<ul class="list pl0 mv4">
+						<li class="f3 deep-purple pointer">Can I add streaming links?</li>
+						<li class="dn f4">Answer</li>
+						</ul>
+						<ul class="list pl0 mv4">
+						<li class="f3 deep-purple pointer">Can I set admin permissions?</li>
+						<li class="dn f4">Answer</li>
+						</ul>
+						<ul class="list pl0 mv4">
+						<li class="f3 deep-purple pointer">Why isn't my playlist permanent?</li>
+						<li class="dn f4">Answer</li>
+						</ul>
+					</ul>
+					</div>
+					<div class="flex flex-column items-center justify-around w-100 min-vh-100">
+					<div class="flex flex-column items-start justify-between w-100 mw7 tl ">
+					<h2 class="f3 ttu uppercase dark-gray mv0">CREDITS</h2>
+						<ul class="list pl0 dark-gray">
+							<ul class="list pl0 mv4">
+							<li class="f3 deep-purple">Code</li>
+							<li class="f4"><a class="link dark-gray" href="https://rsms.me/inter/" target="_blank">Hunor Karamán <span class="deep-purple">↗</span> </a></li>
+							</ul>
+							<ul class="list pl0 mv4">
+							<li class="f3 deep-purple">Design</li>
+							<li class="f4"><a class="link dark-gray" href="https://rsms.me/inter/" target="_blank">Alex Singh<span class="deep-purple">↗</span> </a></li>
+							</ul>
+							<ul class="list pl0 mv4">
+							<li class="f3 deep-purple">Type</li>
+								<li class="f4">
+									<a class="link dark-gray" href="https://rsms.me/inter/" target="_blank">Inter UI <span class="deep-purple">↗</span> </a>
+								</li>
+							</ul>
+						</ul>
+						</div>
 				</div>
 			</body>
 		`
