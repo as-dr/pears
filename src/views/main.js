f99dfc87ee473d455b6748afd64ff6ed1efde05c
Alex Singh <alex@alexsingh.com>
2019-01-21 16:38:24 -0200

    Fixed height to allow for scrolling of large playlists

--- src/views/main.js
+++ src/views/main.js
@@ -15,24 +15,24 @@ function view(state, emit) {
   var waiting = !state.hangtime.playing && (state.hangtime.position < state.hangtime.list.length - 1)
 	return html`
 		<body class="flex flex-row w-100 ph3 f3 lh-copy items-center justify-center" ondragover="${showUploader}">
-			<div class="flex flex-column justify-between items-start w-100 h-100 mw7">
-			  <a href="#" class="link flex pv4 gray" onclick="${back}">Leave</a>
+			<div class="flex flex-column justify-start items-start w-100 min-vh-100 mw7">
+        <a href="#" class="link link-mini bb bw1 pb1 flex mt4 mb5 gray" onclick="${back}">Leave</a>
 
-				<div class="flex flex-column h-100 items-start self-start">
-				  ${state.hangtime.peers.length + 1} LISTENER${state.hangtime.peers.length != 0 ? 'S' : ''}
+				<div class="flex flex-column w-100 h-100 items-start self-center">
+				  ${state.hangtime.peers.length + 1} Listener ${state.hangtime.peers.length != 0 ? 'S' : ''}
 			    ${inviteButton.render()}
 
-  				<ul class="list flex flex-row w-100 pl0 f4 mv4">
+  				<ul class="list flex flex-row w-100 pl0 f3 mv5">
   				  <li class="ml0 mr4">
-  				    <a href="#" class="link mini-links deep-purple  bb pb1 bw1" onclick="${toggleMute}">${state.hangtime.muted ? 'Unmute' : 'Mute'}</a>
+  				    <a href="#" class="link link-mini deep-purple bb pb1 bw1" onclick="${toggleMute}">${state.hangtime.muted ? 'Unmute' : 'Mute'}</a>
   				  </li>
   				</ul>
-			    <div class="flex flex-column w-100 h-auto mw7">
-			      <a href="#" class="link f3 deep-purple" onclick="${openFileDialog}">+ Add Song</a>
+          <a href="#"class="link link-mini w-auto bb bw1 f3 deep-purple mw5 mv2" onclick="${openFileDialog}">Add Song</a>
+			    <div class="flex flex-column w-100 h-auto mw7 pb4 self-center justify-center">
 				    ${playlist.render(state.hangtime.list, state.hangtime.position, waiting)}
 			    </div>
 			  </div>
-		  </div>
+      </div>
 		  ${fileupload.render()}
 		</body>
 	`
